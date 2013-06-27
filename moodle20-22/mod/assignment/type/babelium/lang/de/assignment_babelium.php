8b88b8214de57ce546ba74f5e6f7b8fd5ad1dbf4
Inko Perurena <inkoperurena@gmail.com>
2013-06-27 13:28:29 +0200

    Added contributed German locale to the different plugin versions.

---
+++ moodle20-22/mod/assignment/type/babelium/lang/de/assignment_babelium.php
@@ -0,0 +1,27 @@
+<?php
+
+//Avoid validation warnings
+$string['pluginname'] = 'Aufgabe Babelium';
+
+$string['typebabelium'] = 'Aufgabe Babelium';
+$string['babeliumAvailableRecordableExercises'] = 'Übungen die zur Verfügung stehen';
+$string['babeliumAvailableRecordableExercises_help'] = 'Wählen Sie die Übung, die  Ihren Schülern zur weiteren Beurteilung zuweisen möchten';
+$string['babeliumNoExerciseAvailable'] = 'Keine Tests verfügbar';
+$string['babeliumChooseRole']='Wählen Sie eine Rolle aus';
+$string['babeliumChooseSubLang']='Wählen sie eine Untertitelsprache aus';
+$string['babeliumChooseRecMethod']='Wählen Sie eine Aufnahmemethode aus';
+$string['babeliumMicOnly']='Nur Mikrofon';
+$string['babeliumWebcamMic']='Webkamera und Mikrofon';
+$string['babeliumStartRecording']='Aufnahme beginnen';
+$string['babeliumStopRecording']='Aufnahme unterbrechen';
+$string['babeliumViewRecording']='Aufnahme ansehen';
+$string['babeliumRecordAgain']='Noch einmal aufnehmen';
+$string['babeliumViewExercise']='Übung ansehen';
+
+//Error messages
+$string['babeliumErrorConfigParameters']='Working Parameter Babelium nicht hergestellt';
+$string['babeliumApiErrorCode200']='Babelium Fehlermeldung 200. Fehlerhafte Antwort';
+$string['babeliumApiErrorCode400']='Babelium Fehlermeldung 400. fehlerhaften Anfrage';
+$string['babeliumApiErrorCode403']='Babelium Fehlermeldung 403. Berechtigungsnachweise nicht korrekt';
+$string['babeliumApiErrorCode404']='Babelium Fehlermeldung 404. Methode nicht gefunden. Parameter ungültig';
+$string['babeliumApiErrorCode500']='Babelium Fehlermeldung 500. Interner Serverfehler';
